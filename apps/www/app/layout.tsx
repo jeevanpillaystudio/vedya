04a58232e2ec1dfc35fb27e4ab44840a08b667cd
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-07-18 00:29:58 +0200

    better config system in apps/www

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "apps/www", // @todo fix
-  description: "apps/www", // @todo fix
+  title: 'apps/www', // @todo fix
+  description: 'apps/www', // @todo fix
 };
 
 export default function RootLayout({
--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,9 +1,9 @@
-import ThreeScene from "./_components/three-scene";
+import ThreeScene from './_components/three-scene';
 
 export default function Home() {
   return (
-      <main style={{ width: '100vw', height: '100vh' }}>
+    <main style={{ width: '100vw', height: '100vh' }}>
         <ThreeScene />
-      </main>
+    </main>
   );
 }
