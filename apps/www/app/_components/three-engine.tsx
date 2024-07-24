d5f309e5479e0640a8c1f090857be1ce2de88edb
jeevanpillay <169354619+jeevanpillay@users.noreply.github.com>
2024-07-24 10:26:02 +0200

    better seperation of concerns for viewcube; i think

--- apps/www/app/_components/three-engine.tsx
+++ apps/www/app/_components/three-engine.tsx
@@ -5,7 +5,8 @@ import { Canvas } from "@react-three/fiber";
 import { type EngineCoreSettings, EngineProvider } from "./three-engine-provider";
 import Scene from "./scene";
 import { useRef } from "react";
-import ViewCubeController from "./three-viewcube";
+import type ViewCubeController from "./three-viewcube";
+import Viewcube from "./viewcube";
 
 interface ThreeEngineProps extends EngineCoreSettings {
   name: string;
@@ -27,63 +28,15 @@ const ThreeEngine: React.FC<ThreeEngineProps> = ({ resolution, constructionPlane
         }}
         orthographic
       >
-        <Scene viewCubeControllerRef={vcControllerRef} />
+        <Scene vcControllerRef={vcControllerRef} />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <OrbitControls enableDamping={false} />
       </Canvas>
-
-      <div
-        id="viewcube-container"
-        style={{
-          width: "120px",
-          height: "120px",
-          margin: "10px",
-          perspective: "600px",
-          position: "absolute",
-          right: "60px",
-          bottom: "40px",
-          zIndex: 2,
-        }}
-      >
-        <div
-          id="cube"
-          className="cube"
-          style={{ width: "100px", height: "100px", position: "relative", transformStyle: "preserve-3d", transform: "translateZ(-300px)" }}
-        >
-          {Object.values(ViewCubeController.CubeOrientation).map((orientation) => (
-            <div
-              key={orientation}
-              className={`cube__face cube__face--${orientation}`}
-              style={{
-                width: "100px",
-                height: "100px",
-                border: "2px solid #808080",
-                lineHeight: "100px",
-                fontSize: "25px",
-                fontWeight: "bold",
-                color: "#7d7d7d",
-                textAlign: "center",
-                background: "#fff",
-                cursor: "pointer",
-                userSelect: "none",
-                position: "absolute",
-                transform: `rotateX(${ViewCubeController.ORIENTATIONS[orientation]!.rotationX}deg) rotateY(${ViewCubeController.ORIENTATIONS[orientation]!.rotationY}deg) translateZ(50px)`,
-              }}
-              onClick={() => {
-                if (!vcControllerRef.current) {
-                  return;
-                }
-                vcControllerRef.current.tweenCamera(ViewCubeController.ORIENTATIONS[orientation]!);
-              }}
-            >
-              {orientation}
-            </div>
-          ))}
-        </div>
-      </div>
+      <Viewcube vcControllerRef={vcControllerRef} />
     </EngineProvider>
   );
 };
 
 export default ThreeEngine;
+
--- apps/www/app/_components/viewcube.tsx
+++ apps/www/app/_components/viewcube.tsx
@@ -1,72 +1,64 @@
-"use client";
+// @todo refactor styles to clsx
 
-import React, { useRef, forwardRef, useImperativeHandle } from "react";
-import { Hud, OrthographicCamera, RenderTexture, Text } from "@react-three/drei";
-import { useThree, useFrame, type MeshProps } from "@react-three/fiber";
-import { type Mesh, Matrix4 } from "three";
+import { type MutableRefObject } from "react";
+import ViewCubeController from "./three-viewcube";
 
-const Viewcube: React.FC = () => {
-  const renderPriority = 1;
-  const matrix = new Matrix4();
-  const mesh = useRef<Mesh>(null);
-  const { camera } = useThree();
+interface ViewcubeInterface {
+  vcControllerRef: MutableRefObject<ViewCubeController | undefined>;
+}
 
-  useFrame(() => {
-    // Spin mesh to the inverse of the default camera's matrix
-    matrix.copy(camera.matrix).invert();
-
-    if (mesh.current) {
-      mesh.current.quaternion.setFromRotationMatrix(matrix);
-    }
-  });
-
+const Viewcube: React.FC<ViewcubeInterface> = ({ vcControllerRef }) => {
   return (
-    <Hud renderPriority={renderPriority}>
-      <Box ref={mesh} position={[0, 0, 0]}>
-        <ViewcubeFace index={0}>FRONT</ViewcubeFace>
-        <ViewcubeFace index={1}>BACK</ViewcubeFace>
-        <ViewcubeFace index={2}>TOP</ViewcubeFace>
-        <ViewcubeFace index={3}>BOTTOM</ViewcubeFace>
-        <ViewcubeFace index={4}>LEFT</ViewcubeFace>
-        <ViewcubeFace index={5}>RIGHT</ViewcubeFace>
-      </Box>
-    </Hud>
+    <div
+      id="viewcube-container"
+      style={{
+        width: "120px",
+        height: "120px",
+        margin: "10px",
+        perspective: "600px",
+        position: "absolute",
+        right: "60px",
+        bottom: "40px",
+        zIndex: 2,
+      }}
+    >
+      <div
+        id="cube"
+        className="cube"
+        style={{ width: "100px", height: "100px", position: "relative", transformStyle: "preserve-3d", transform: "translateZ(-300px)" }}
+      >
+        {Object.values(ViewCubeController.CubeOrientation).map((orientation) => (
+          <div
+            key={orientation}
+            className={`cube__face cube__face--${orientation}`}
+            style={{
+              width: "100px",
+              height: "100px",
+              border: "2px solid #808080",
+              lineHeight: "100px",
+              fontSize: "25px",
+              fontWeight: "bold",
+              color: "#7d7d7d",
+              textAlign: "center",
+              background: "#fff",
+              cursor: "pointer",
+              userSelect: "none",
+              position: "absolute",
+              transform: `rotateX(${ViewCubeController.ORIENTATIONS[orientation]!.rotationX}deg) rotateY(${ViewCubeController.ORIENTATIONS[orientation]!.rotationY}deg) translateZ(50px)`,
+            }}
+            onClick={() => {
+              if (!vcControllerRef.current) {
+                return;
+              }
+              vcControllerRef.current.tweenCamera(ViewCubeController.ORIENTATIONS[orientation]!);
+            }}
+          >
+            {orientation}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
 export default Viewcube;
-
-interface BoxProps extends MeshProps {
-  children: React.ReactNode;
-}
-
-const Box = forwardRef<Mesh, BoxProps>(({ children, ...props }, ref) => {
-  const meshRef = useRef<Mesh>(null);
-  useImperativeHandle(ref, () => meshRef.current!);
-  return (
-    <mesh {...props} ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      {children}
-    </mesh>
-  );
-});
-
-Box.displayName = "ViewcubeBox";
-
-interface FaceMaterialProps {
-  index: number;
-  children: React.ReactNode;
-}
-
-const ViewcubeFace: React.FC<FaceMaterialProps> = ({ children, index }) => {
-  return (
-    <meshStandardMaterial attach={`material-${index}`} color={"orange"}>
-      <RenderTexture attach="map" anisotropy={16}>
-        <OrthographicCamera makeDefault position={[0, 0, 5]} zoom={50} />
-        <Text fontSize={0.4} color="black" anchorX="center" anchorY="middle">
-          {children}
-        </Text>
-      </RenderTexture>
-    </meshStandardMaterial>
-  );
-};
